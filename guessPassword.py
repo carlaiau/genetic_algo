7a0953d73de60c1ae48622372c17aabb684f6a76
Carl Aiau <carlaiau@Carls-MBP.student.uod.otago.ac.nz>
2017-04-26 17:38:55 +1200

    1.3 Initial Task, procedural

---
+++ guessPassword.py
@@ -0,0 +1,46 @@
+import random
+import datetime
+
+geneSet = "1234567890 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,'()/?"
+target = "Haven't been doing much ae. Course starts in 3 weeks. Will have to get into some study to prepare"
+
+def generate_parent(length):
+    genes = []
+    while len(genes) < length:
+        sampleSize = min(length - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, sampleSize ))
+    return ''.join(genes)
+
+
+def get_fitness(guess):
+    return sum(1 for expected, actual in zip(target, guess)
+               if expected == actual)
+
+def mutate(parent):
+    index = random.randrange(0, len(parent))
+    childGenes = list(parent)
+    newGene, alternate = random.sample(geneSet, 2)
+    childGenes[index] = alternate if newGene == childGenes[index] else newGene
+    return ''.join( childGenes )
+
+def display(guess):
+    timeDiff = datetime.datetime.now() - startTime
+    fitness = get_fitness( guess )
+    print( "{}\t{}\t{}\t".format(fitness, timeDiff, guess ) )
+
+random.seed()
+startTime = datetime.datetime.now()
+bestParent = generate_parent( len( target ) )
+bestFitness = get_fitness( bestParent )
+display( bestParent )
+
+while True:
+    child = mutate( bestParent )
+    childFitness = get_fitness( child )
+    if bestFitness >= childFitness:
+        continue
+    display( child )
+    if childFitness >= len( bestParent ):
+        break
+    bestFitness = childFitness
+    bestParent = child
